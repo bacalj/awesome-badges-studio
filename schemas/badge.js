16da0c9a20d5b3dc0cd3123346aa8db470e40789
Joseph Bacal <bacalj@gmail.com>
2021-04-03 14:00:09 -0400

    initial schema setup

---
+++ schemas/badge.js
@@ -0,0 +1,35 @@
+export default {
+    name: "badge",
+title: "Badges",
+type: "document",
+fields: [
+        {
+                        name: "title",
+            title: "Title",
+            type: "string",
+        },
+        {
+            name: "description", 
+            title: "Description",
+            type: "array", 
+            of: [
+            { type: 'block' }, 
+            ]
+        },
+        {
+            name: 'badgeImage',
+                        title: 'Badge Image',
+            type: 'image'
+        },
+        {
+            name: "slug",
+            title: "Slug",
+            type: "slug",
+            hidden: true,
+            options: {
+                source: 'name',
+                maxLength: 96
+            }
+        },
+    ]
+}
---
+++ schemas/student.js
@@ -0,0 +1,22 @@
+export default {
+    name: "student",
+    title: "Students",
+    type: "document",
+    fields: [
+        {
+            name: "gSuiteName",
+            title: "GSuite Username",
+            type: "string",
+        },
+        {
+            name: "name",
+            title: "Name",
+            type: "string",
+        },
+        {
+            name: 'avatar',
+            title: 'Avatar',
+            type: 'image'
+        },
+    ]
+}
