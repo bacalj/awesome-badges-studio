e69803e0430ed0ac9a2cb479c01428b5b0180660
Joseph Bacal <bacalj@gmail.com>
2021-04-10 13:10:54 -0400

    adding the settings page

--- schemas/schema.js
+++ schemas/schema.js
@@ -1,12 +1,13 @@
 import createSchema from 'part:@sanity/base/schema-creator'
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 
+import siteSettings from './siteSettings'
 import badge from './badge'
 import student from './student'
 
 export default createSchema({
   name: 'default',
   types: schemaTypes.concat([
-    badge, student
+    badge, student, siteSettings
   ])
 })
---
+++ schemas/siteSettings.js
@@ -0,0 +1,19 @@
+// schemas/siteSettings.js
+export default {
+    name: 'siteSettings',
+    title: 'Site Settings',
+    type: 'document',
+    __experimental_actions: [/*'create',*/ 'update', /*'delete',*/ 'publish'], 
+    fields: [
+      {
+        name: 'title',
+        title: 'Site Title',
+        type: 'string'
+      },
+      {
+        name: 'description',
+        title: 'Site Description',
+        type: 'text'
+      }
+    ]
+  }
